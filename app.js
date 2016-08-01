b25cb6483cc09f26ccfa5c66b4d563e39a5fb197
morgan-ashley <morgan.angela.ashley@gmail.com>
2016-08-01 09:21:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).on('ready', function() {
+
+    var winHeight = $(window).height(),
+        docHeight = $(document).height(),
+        progressBar = $('progress'),
+        max, value;
+
+    /* Sets max scrollable area */
+    max = docHeight - winHeight;
+    progressBar.attr('max', max);
+
+    $(document).on('scroll', function() {
+        value = $(window).scrollTop();
+        progressBar.attr('value', value);
+
+    });
+    $("img").click(function() {
+        $(this).attr("src", "assets/images/complete_on.png");
+        console.log('im working!');
+
+    });
+});
