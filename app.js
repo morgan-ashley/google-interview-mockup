09d7a0e13d80ba30572d314e351b1fc35797fce6
Morgan Ashley <morganangelaashley@gmail.com>
2016-08-02 11:20:32 -0700

    Update
    JS for animated scroll on side menu

--- app.js
+++ app.js
@@ -15,8 +15,52 @@ $(document).on('ready', function() {
 
     });
     $('img').click(function() {
-        $(this).attr('src', 'assets/images/complete_on.png');
+        $(this).attr("src", "assets/images/complete_on.png");
         console.log('im working!');
-
     });
+});
+var lastId,
+    sideMenu = $(".nav"),
+    sideMenuHeight = sideMenu.outerHeight() + 15,
+    // All anchor side bar tags
+    menuItems = sideMenu.find("a"),
+    // Anchors corresponding to list items
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
+
+// Bind click handler to list items for scroll animation
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - sideMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function() {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + sideMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
 });
